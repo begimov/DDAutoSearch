54a22fa4f2e49fdabe3f4f7999cb15743b9063b3
begimov <begimov@gmail.com>
2017-04-05 15:59:07 +0300

    dropdown with autocomplete using algolia and autocomplete-js added

--- resources/assets/js/components/Message.vue
+++ resources/assets/js/components/Message.vue
@@ -1,36 +1,44 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Message</div>
-
-                    <div class="panel-body">
-                        <form action="#" v-on:submit.prevent="send">
-                          <div class="form-group">
-                            <label for="Users">Select users</label>
-                            <input type="text" name="users" value="users" class="form-control">
-                          </div>
-                          <div class="form-group">
-                            <button type="submit" class="btn btn-primary">Send</button>
-                          </div>
-                        </form>
-                    </div>
-                </div>
+<div class="panel panel-default">
+    <div class="panel-heading">Message</div>
+    <div class="panel-body">
+        <form action="#" v-on:submit.prevent="send">
+            <div class="form-group">
+                <label for="Users">Select users</label>
+                <input type="text" name="users" id="users" class="form-control">
             </div>
-        </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Send</button>
+            </div>
+        </form>
     </div>
+</div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        },
-        methods: {
-          send () {
+import autocomplete from 'autocomplete.js'
+import algolia from 'algoliasearch'
+
+export default {
+    mounted() {
+        const index = algolia('O8MEW55J9E', '343230ff7c66bd77e98c352ffec8ee8a').initIndex('users')
+        autocomplete('#users', {}, {
+            source: autocomplete.sources.hits(index, {
+                hitsPerPage: 5
+            }),
+            displayKey: 'name',
+            templates: {
+                suggestion(suggestion) {
+                    return `<span>${suggestion.name}</span>`
+                },
+                empty: '<div class="aa-empty">No users found</div>'
+            }
+        })
+    },
+    methods: {
+        send() {
             console.log('SUBMIT')
-          }
         }
     }
+}
 </script>

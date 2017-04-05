f086c978ffd0fac3d9b5128c0e15e458fc2cbf17
begimov <begimov@gmail.com>
2017-04-05 14:47:18 +0300

    vue component added

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,7 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('message', require('./components/Message.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/Message.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Message</div>
+
+                    <div class="panel-body">
+                        <form action="#" v-on:submit.prevent="send">
+                          <div class="form-group">
+                            <label for="Users">Select users</label>
+                            <input type="text" name="users" value="users" class="form-control">
+                          </div>
+                          <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Send</button>
+                          </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        methods: {
+          send () {
+            console.log('SUBMIT')
+          }
+        }
+    }
+</script>
